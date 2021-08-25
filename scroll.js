a771b94b54a70452178cf2f3d6c36aabe3c53fa2
paluzz <65746980+paluzz@users.noreply.github.com>
2021-08-25 04:27:57 -0300

    arreglo de scroll

--- scroll.js
+++ scroll.js
@@ -4,4 +4,3 @@ ScrollReveal().reveal('.cards-banner-one', {delay: 300});
 ScrollReveal().reveal('.cards-banner-two', {delay: 300});
 ScrollReveal().reveal('.social', {delay: 500});
 ScrollReveal().reveal('.footer-links', {delay: 500});
-ScrollReveal().reveal('.footer', {delay: 500});
