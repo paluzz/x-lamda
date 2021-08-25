5cc3ff85c9e8234f8f43b49a5bb1e52230094ae8
paluzz <65746980+paluzz@users.noreply.github.com>
2021-08-25 04:02:56 -0300

    first commit

---
+++ scroll.js
@@ -0,0 +1,7 @@
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.new-cards', {delay: 300});
+ScrollReveal().reveal('.cards-banner-one', {delay: 300});
+ScrollReveal().reveal('.cards-banner-two', {delay: 300});
+ScrollReveal().reveal('.social', {delay: 500});
+ScrollReveal().reveal('.footer-links', {delay: 500});
+ScrollReveal().reveal('.footer', {delay: 500});
